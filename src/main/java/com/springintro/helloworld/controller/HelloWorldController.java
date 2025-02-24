d2667944f40f63b1f2acd80a45fec91d88c2eb0f
kanchansingh2004 <kannursingh2004@gmail.com>
2025-02-24 10:51:12 +0530

    [Kanchan] Add. UC-02 log different levels of messages when /api/log is accessed

--- src/main/java/com/springintro/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/springintro/helloworld/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.springintro.helloworld.controller;
 
 import com.springintro.helloworld.component.EmployeeBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +29,18 @@ public class HelloWorldController {
         return "Employee ID: " + employeeBean.getId() + ", "
                 + "Employee Name: " + employeeBean.getName() + ", "
                 + "Department: " + employeeBean.getDeptName();
+    }
+
+    //creating a logger instance for this class
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+    //method to log different levels of messages when "/api/log" is accessed
+    @GetMapping("/log")
+    public String logMessages() {
+        logger.info("Information of logMessages() method");
+        logger.debug("Debugging log");
+        logger.warn("warning");
+        logger.error("ERROR");
+        //returning a response to indicate logs have been recorded
+        return "Logs have been recorded.";
     }
 }
